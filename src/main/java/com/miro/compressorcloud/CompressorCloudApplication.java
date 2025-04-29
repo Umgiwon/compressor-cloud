d37e0aa43d3750849955e851669121bdab526457
umgiwon <ugwzzang@gmail.com>
2025-04-29 17:43:32 +0900

    # init

---
+++ src/main/java/com/miro/compressorcloud/CompressorCloudApplication.java
@@ -0,0 +1,13 @@
+package com.miro.compressorcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompressorCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompressorCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miro/compressorcloud/CompressorCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.compressorcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompressorCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
